50f0e12a4e33104ad4c5bf5c3877c0be5e3b144a
rjmarin <rjmarin@miuandes.cl>
2018-10-25 21:24:47 -0300

    perfil listo

--- api.py
+++ api.py
@@ -25,10 +25,12 @@ while main:
 
             cur.execute(("SELECT  * FROM clave WHERE {} = email_usuario ORDER BY fecha DESC LIMIT 1;  ").format(str("'" + email + "'")))
             rows = cur.fetchall()
+            cur.execute("SELECT activo from usuario WHERE email = {};".format(str("'" + email + "'")))
+            activo = cur.fetchone()
             usarioE = False
             while True:
-                for i in rows:
-                    if i[0]==email and i[1] == contrasena:                 
+                for i in rows:  
+                    if i[0]==email and i[1] == contrasena and activo[0] == True:                 
                         print("Sesion iniciada")
                         cur.execute(("SELECT id FROM perfil WHERE {} = email;").format(str("'" + email + "'")))
                         id = cur.fetchone()
@@ -124,7 +126,6 @@ while TRUUEEE:
             cur.execute(" SELECT  habilidad_perfil, COUNT(*) FROM validar WHERE id_perfil = {} GROUP BY habilidad_perfil".format(perfil[0]))
             habilidad = cur.fetchall()
             print(tabulate(habilidad, headers=["habilidades", "validaciones"], tablefmt= "fancygrid"))
-
             print("-----------------------------------")
             print("\t MENU PERFIL ")
             print("(1) EDITAR PERFIL")
@@ -175,7 +176,7 @@ while TRUUEEE:
                     elif cambioperfil == "6":
                         break
                     else:
-                        print("opcion incorrecta! Intente nueamente")
+                        print("opcion incorrecta! Intente nuevamente")
                 
                 pass
             elif opcionperfil=="2":
@@ -184,13 +185,23 @@ while TRUUEEE:
                 
             elif opcionperfil=="3":
                 while True:
-                    cur.execute(" SELECT  habilidad_perfil, COUNT(*) FROM validar WHERE id_perfil = {} GROUP BY habilidad_perfil".format(perfil[0]))
+                    cur.execute(" SELECT  habilidad, descripcion  FROM habilidad WHERE id_perfil = {}  ORDER BY habilidad".format(perfil[0]))
                     habilidad = cur.fetchall()
+                    cur.execute("SELECT habilidad_perfil , COUNT(*) FROM validar WHERE id_perfil = {} GROUP BY habilidad_perfil ORDER BY habilidad_perfil".format(perfil[0]))
+                    validation = cur.fetchall()
                     count = 1
                     hab =[]
                     for hh in habilidad:
-                        hab.append([count, hh[0], hh[1]])
-                        count += 1
+                        func = True
+                        for val in validation:
+                            if hh[0] in val:
+                                hab.append([count, val[0], val[1]])
+                                count +=1
+                                func = False
+                                break
+                        if func:
+                            hab.append([count,hh[0], 0])
+                            count += 1
                     print(tabulate(hab, headers=["numero", "habilidades", "validaciones"], tablefmt= "fancygrid"))
                     print("(1) VER HABILIDAD")
                     print("(2) AGREGAR HABILIDAD")
@@ -199,17 +210,30 @@ while TRUUEEE:
                     opcionhabilidad = raw_input("ingrese una opcion:")
                     if opcionhabilidad == "1":
                         numh = int(raw_input("ingrese numero de habilidad: "))
-
-                        cur.execute(" SELECT habilidad_perfil, email_usuario_valida FROM validar WHERE id_perfil = {} and habilidad_perfil = {}".format(perfil[0], str("'" +  hab[0][numh] + "'")))
+                        print("NOMBRE habilidad: " + hab[numh-1][1] +" \ndescricion: " + habilidad[numh-1][1] )
+                        cur.execute(" SELECT habilidad_perfil, email_usuario_valida FROM validar  WHERE id_perfil = {} and habilidad_perfil = {}".format(perfil[0], str("'" +  hab[numh-1][1] + "'")))
                         emails = cur.fetchall()
-                        print(tabulate(emails, headers=[ "habilidad", "usuario que valido"], tablefmt= "fancygrid"))
+                        if emails == []:
+                            print("NO TIENE VALIDACIONES")
+                        else:
+                            print(tabulate(emails, headers=[ "habilidad", "usuario que valido"], tablefmt= "fancygrid"))
                     elif opcionhabilidad == "2":
                         nuevahab = raw_input("ingrese  nombre de una nueva habilidad: ")
                         deschab = raw_input("ingrse descripcion de esta: ")
-                        cur.execute("INSERT INTO Habilidad(id_perfil,habilidad,descripcion) VALUES ({},{},{})".format(id[0],nuevahab, deschab ))
-                        conn.commit
+                        nuevahab = str("'" + nuevahab + "'")
+                        deschab = str("'" + deschab + "'" ) 
+                        cur.execute("INSERT INTO Habilidad(id_perfil,habilidad,descripcion) VALUES ({},{},{})".format(perfil[0],nuevahab, deschab ))
+                        conn.commit()
                     elif opcionhabilidad == "3":
-                        ophabilidad = raw_input("ingrese numero de habilidad")
+                        ophabilidad = int(raw_input("ingrese numero de habilidad: "))
+                        for h in hab:
+                            if ophabilidad == h[0]:
+                                cur.execute("DELETE  from  validar WHERE id_perfil = {} and habilidad_perfil = {}".format(id[0], str("'"+h[1]+"'")))
+                                conn.commit()
+                                cur.execute("DELETE  from habilidad WHERE id_perfil = {} and habilidad = {}".format(id[0], str("'"+h[1]+"'")))
+                                conn.commit()
+                                
+
                     elif opcionhabilidad == "4":
                         break              
                     else:
@@ -220,18 +244,81 @@ while TRUUEEE:
             elif opcionperfil=="4":
                 pass
             elif opcionperfil=="5":
-                pass
+                while True:
+                    cur.execute("SELECT * FROM  estudio  WHERE id_perfil = {}".format(perfil[0]))
+                    estudio = cur.fetchall()
+                    c = 1
+                    estu =[]
+                    print("\tESTUDIOS")
+                    for e in estudio:
+                        estu.append([c, e[0], e[1], e[2], e[3], e[4], e[5]])
+                        print("(" + str(c) + ")  " + e[3] + " en " + e[1])
+                        c += 1
+                    print("------------------------")
+                    print("(1) VER EDUCACION")
+                    print("(2) AGREGAR EDUCACION")
+                    print("(3) ELIMINAR EDUCACION")
+                    print("(4) VOLVER")
+                    print("------------------")
+                    opcionestudio = raw_input("Ingrese una opcion: ")
+                    if opcionestudio == "1":
+                        numero_estudio = int(raw_input("ingrese un numero de la educacion: "))
+                        for es in estu:
+                            if numero_estudio ==  es[0]:
+                                print("nombre institucion: " + es[2] + "\ngrado academico: " + es[3] + "\nestudio: " + es[4] +
+                                 "\nfecha de inicio: " + str(es[5]) + "\nfecha de fin: " + str(es[6]))
+
+                    elif opcionestudio == "2":
+                        cur.execute("SELECT  * from institucion")
+                        instituciones = cur.fetchall()
+                        institucion = []
+                        c=1
+                        for ins in instituciones:
+                            institucion.append([c, ins[0], ins[1], ins[2] , ins[3]])
+                            print("(" + str(c) + ") " + ins[0])
+                            c += 1
+                        numinst= int(raw_input("Ingrese un numero de una institucion: "))
+                        for insti in institucion:
+                            if numinst == insti[0]:
+                                grado = raw_input("Ingrese grado academico: ")
+                                descripcion = raw_input("Ingrese estudio: ")
+                                fecha_inicio = raw_input("ingrese fecha de inicio(YY-MM-DD): ")
+                                fecha_termino = raw_input("ingrese fecha de fin(YY-MM-DD): ") 
+                                cur.execute("INSERT INTO Estudio(id_perfil,nombre_institucion,grado_academico,descripcion,fecha_inicio,fecha_fin) VALUES ({},'{}','{}','{}','{}','{}');".format(perfil[0], insti[1], grado, descripcion,fecha_inicio,fecha_termino))
+                                conn.commit()
+
+                    elif opcionestudio == "3":
+                        opcionestudio = raw_input("Ingrese numero de estudio")
+                        for est in estu:
+                            if opcionestudio == str(est[0]):
+                                cur.execute("DELETE  FROM  estudio WHERE id_perfil = {} and nombre_institucion = {} and grado_academico = {}". format(perfil[0],str("'"+est[2]+"'"),str("'"+est[3]+"'") ))
+                                conn.commit()
+                        
+                    elif opcionestudio == "4":
+                        break
+                    else:
+                        print("Opcion incorrecta! Intente nuevamente!")
+
             elif opcionperfil=="6":
-                pass
+                print("seguro desea borrar la cuenta (1)SI (2)NO :")
+                borrar = raw_input()
+                if borrar == "1":
+                    cur.execute("UPDATE usuario SET activo = false  WHERE email = {};".format(str("'"+perfil[1]+"'")))
+                    conn.commit()
+                    TRUUEEE = False
+                    break
+                else:
+                    break
+
+                
             elif opcionperfil== "7":
                 Bperfil = False
             else:
                  print("Opcion invalida")
-                 
 
+    elif opcion == "2":
 
-    elif opcion == "2": 
-        pass
+        
     elif opcion == "3":
         Boolnotiifcaciones = True
         while Boolnotiifcaciones:
